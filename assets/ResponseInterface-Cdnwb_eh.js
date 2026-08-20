dc82c867a870a2b1650e557ca5f6f36f69d3010c
DevBot CI <ci@devcraft.club>
2026-08-20 11:04:37 +0000

    Deploy 2026-08-20T11:04:16Z

---
+++ assets/ResponseInterface-Cdnwb_eh.js
@@ -0,0 +1,59 @@
+import{r as e}from"./rolldown-runtime-QTnfLwEv.js";import{t}from"./jsx-runtime-By8HlURe.js";var n=e(t()),r={title:`Интерфейс: ResponseInterface`,description:`Документация по плагину интерфейс: responseinterface для DLE.`},i=new Date(1787223655e3),a=`
+
+## Краткое содержание: [#краткое-содержание]
+
+Контракт HTTP-ответа, отправляемого клиенту.
+
+***
+
+### Свойства [#свойства]
+
+*Нет публичных свойств.*
+
+***
+
+### Методы [#методы]
+
+* public [send()](#method_send)
+
+***
+
+### Подробности [#подробности]
+
+* Путь: \`devcraft/src/classes/Interfaces/ResponseInterface.php\`
+
+***
+
+## Детали [#детали]
+
+### Методы [#методы-1]
+
+<span id="method_send" />
+
+### send() [#send]
+
+Отправляет сформированный ответ клиенту.
+
+**С версии:** 200.4.0
+`,o={contents:[{heading:`краткое-содержание`,content:`Контракт HTTP-ответа, отправляемого клиенту.`},{heading:`свойства`,content:`*Нет публичных свойств.*`},{heading:`методы`,content:`public send()`},{heading:`подробности`,content:"Путь: `devcraft/src/classes/Interfaces/ResponseInterface.php`"},{heading:`send`,content:`Отправляет сформированный ответ клиенту.`},{heading:`send`,content:`**С версии:** 200.4.0`}],headings:[{id:`краткое-содержание`,content:`Краткое содержание:`},{id:`свойства`,content:`Свойства`},{id:`методы`,content:`Методы`},{id:`подробности`,content:`Подробности`},{id:`детали`,content:`Детали`},{id:`методы-1`,content:`Методы`},{id:`send`,content:`send()`}]},s=[{depth:2,url:`#краткое-содержание`,title:(0,n.jsx)(n.Fragment,{children:`Краткое содержание:`})},{depth:3,url:`#свойства`,title:(0,n.jsx)(n.Fragment,{children:`Свойства`})},{depth:3,url:`#методы`,title:(0,n.jsx)(n.Fragment,{children:`Методы`})},{depth:3,url:`#подробности`,title:(0,n.jsx)(n.Fragment,{children:`Подробности`})},{depth:2,url:`#детали`,title:(0,n.jsx)(n.Fragment,{children:`Детали`})},{depth:3,url:`#методы-1`,title:(0,n.jsx)(n.Fragment,{children:`Методы`})},{depth:3,url:`#send`,title:(0,n.jsx)(n.Fragment,{children:`send()`})}];function c(e){let t={a:`a`,code:`code`,em:`em`,h2:`h2`,h3:`h3`,hr:`hr`,li:`li`,p:`p`,strong:`strong`,ul:`ul`,...e.components};return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)(t.h2,{id:`краткое-содержание`,children:`Краткое содержание:`}),`
+`,(0,n.jsx)(t.p,{children:`Контракт HTTP-ответа, отправляемого клиенту.`}),`
+`,(0,n.jsx)(t.hr,{}),`
+`,(0,n.jsx)(t.h3,{id:`свойства`,children:`Свойства`}),`
+`,(0,n.jsx)(t.p,{children:(0,n.jsx)(t.em,{children:`Нет публичных свойств.`})}),`
+`,(0,n.jsx)(t.hr,{}),`
+`,(0,n.jsx)(t.h3,{id:`методы`,children:`Методы`}),`
+`,(0,n.jsxs)(t.ul,{children:[`
+`,(0,n.jsxs)(t.li,{children:[`public `,(0,n.jsx)(t.a,{href:`#method_send`,children:`send()`})]}),`
+`]}),`
+`,(0,n.jsx)(t.hr,{}),`
+`,(0,n.jsx)(t.h3,{id:`подробности`,children:`Подробности`}),`
+`,(0,n.jsxs)(t.ul,{children:[`
+`,(0,n.jsxs)(t.li,{children:[`Путь: `,(0,n.jsx)(t.code,{children:`devcraft/src/classes/Interfaces/ResponseInterface.php`})]}),`
+`]}),`
+`,(0,n.jsx)(t.hr,{}),`
+`,(0,n.jsx)(t.h2,{id:`детали`,children:`Детали`}),`
+`,(0,n.jsx)(t.h3,{id:`методы-1`,children:`Методы`}),`
+`,(0,n.jsx)(`span`,{id:`method_send`}),`
+`,(0,n.jsx)(t.h3,{id:`send`,children:`send()`}),`
+`,(0,n.jsx)(t.p,{children:`Отправляет сформированный ответ клиенту.`}),`
+`,(0,n.jsxs)(t.p,{children:[(0,n.jsx)(t.strong,{children:`С версии:`}),` 200.4.0`]})]})}function l(e={}){let{wrapper:t}=e.components||{};return t?(0,n.jsx)(t,{...e,children:(0,n.jsx)(c,{...e})}):c(e)}export{a as _markdown,l as default,r as frontmatter,i as lastModified,o as structuredData,s as toc};
